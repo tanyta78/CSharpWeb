a91ec606cf037857f81660600782485b326b37b4
Tatyana Milanova <tanyta78@gmail.com>
2018-10-16 18:10:19 +0300

    t

--- HandmadeHTTPServer/WebServer/Application/Views/Home/IndexView.cs
+++ HandmadeHTTPServer/WebServer/Application/Views/Home/IndexView.cs
@@ -4,10 +4,6 @@
     
    public class IndexView:IView
     {
-<<<<<<< HEAD
-        public string View() =>"<h1>Welcome!</h1>";
-=======
         public string View() => "<h1>Welcome</h1>";
->>>>>>> b8e76d80beb0eff0ab4ae9ca15efe2b0b13a1fab
     }
 }
